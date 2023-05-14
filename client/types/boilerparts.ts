01c71386ee5e0da375c7e278b5814351f5646d43
SntSwetlana <snt.swetlana@gmail.com>
2023-05-14 19:46:13 +0300

    test

---
+++ client/types/boilerparts.ts
@@ -0,0 +1,15 @@
+export interface IBoilerPart {
+  boiler_manufacturer: string,
+  price: number,
+  parts_manufacturer: string,
+  vendor_code: string,
+  name: string,
+  description: string,
+  images: string,
+  in_stock: number,
+  bestseller: boolean,
+  new: boolean,
+  popularity: number,
+  compatibility: string,
+
+}
