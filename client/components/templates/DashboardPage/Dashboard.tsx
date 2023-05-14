749ba2b676b15a1c5621d0136997033186de2c9c
SntSwetlana <snt.swetlana@gmail.com>
2023-05-14 19:45:09 +0300

    add boilerpart description

--- client/components/templates/DashboardPage/Dashboard.tsx
+++ client/components/templates/DashboardPage/Dashboard.tsx
@@ -1,7 +1,10 @@
 import BrandSlider from '@/components/modules/DashboardPage/BrandSlider'
 import styles from '@/styles/dashboard/index.module.scss'
+import { useState } from 'react'
 
 const DashboardPage = () => {
+  const [newParts, setNewParts] = useState([])
+  const [bestSellers, setBestSellers] = useState<[]>([])
   console.log()
   return (
     <section className={styles.dashboard}>
